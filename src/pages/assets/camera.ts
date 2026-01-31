bd408542532965138c0ce96796b6841a8179e185
gardalog <126806218+gardalog@users.noreply.github.com>
2026-01-31 22:42:55 +0700

    adjusted turn speed

--- src/pages/assets/camera.ts
+++ src/pages/assets/camera.ts
@@ -1,12 +1,12 @@
 import { Scene, FollowCamera, Vector3 } from '@babylonjs/core';
 
 const defaultHeight = 5;
-const defaultDistance = 15;
+const defaultDistance = 10;
 export function createCamera(scene: Scene, canvas: HTMLCanvasElement) {
     const camera = new FollowCamera("camera", new Vector3(0, defaultHeight, -defaultDistance), scene);
     camera.setTarget(new Vector3(0, 0, 0));
     camera.heightOffset = 5; // Height above the target
-    camera.radius = 15; // Distance from the target
+    camera.radius = 10; // Distance from the target
     // camera.attachControl(canvas, true);
     return camera;
 }
