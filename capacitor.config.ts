d7ec3be8735a7f1c74a2c144e78de76674ddf73b
gardalog <126806218+gardalog@users.noreply.github.com>
2026-02-01 03:21:18 +0700

    pwa image and icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'p2026',
+  appId: 'io.maskedout.app',
+  appName: 'masked-out',
   webDir: 'dist'
 };
 
